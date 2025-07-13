1a5e4fc6e287ec2197ffc235fba52c6363937e26
smurak1201 <skip.to.my.lou.1201@gmail.com>
2025-07-13 15:49:24 +0900

    Appコンポーネントのコードを整理し、コメントを追加して可読性を向上

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
+// Chakra UIからレイアウト用のBoxとテキスト用のTextをインポート
 import { Box, Text } from "@chakra-ui/react";
+// Todoリスト表示用コンポーネント
 import TodoList from "./components/TodoList";
+// Todo追加用コンポーネント
 import TodoInput from "./components/TodoInput";
+// Todoの状態管理用カスタムフック
 import { useTodos } from "./hooks/useTodos";
 
+// アプリのメインコンポーネント
 function App() {
+  // useTodosフックでTodo一覧と操作関数を取得
   const { todos, addTodo, removeTodo, moveTodo, editTodo } = useTodos();
 
   return (
+    // Boxで全体のレイアウトを整える
     <Box
-      maxW="md"
-      mx="auto"
-      mt={4}
-      p={6}
-      bg="white"
-      borderRadius="lg"
-      boxShadow="none"
-      border="none"
+      maxW="md" // 最大幅
+      mx="auto" // 中央寄せ
+      mt={4} // 上部余白
+      p={6} // 内側余白
+      bg="white" // 背景色
+      borderRadius="lg" // 角丸
+      boxShadow="none" // シャドウなし
+      border="none" // 枠線なし
     >
+      {/* タイトル表示 */}
       <Text fontSize="2xl" fontWeight="bold" color="teal.500" mb={4}>
         Todoリスト
       </Text>
+      {/* Todo追加フォーム。onAddで新規Todoを追加 */}
       <TodoInput onAdd={addTodo} />
+      {/* Todo一覧表示。各操作関数をpropsで渡す */}
       <TodoList
         todos={todos}
         onRemove={removeTodo}
@@ -31,4 +41,5 @@ function App() {
   );
 }
 
+// Appコンポーネントをエクスポート
 export default App;
