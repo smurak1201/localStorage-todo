f05a45d409a93a46a0930c27c43248d699bca396
smurak1201 <skip.to.my.lou.1201@gmail.com>
2025-07-13 13:04:20 +0900

    不要なSVGファイルを削除し、Appコンポーネントからロゴ画像のインポートを削除しました

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,12 +6,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <a href="https://vite.dev" target="_blank"></a>
+        <a href="https://react.dev" target="_blank"></a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
