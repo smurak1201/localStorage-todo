be940964e43bb9414f477b26d1e13cbde2fcda7e
smurak1201 <skip.to.my.lou.1201@gmail.com>
2025-08-04 13:41:00 +0900

    公開URLのルートパスを指定する設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 公開URLのルートパスを指定（サブディレクトリ配信の場合は '/localstrage-todo/'）
+  base: '/localstrage-todo/',
 })
